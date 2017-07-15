41a087bd139aeb9e68efe29dfd018f01661928c7
245545357@qq.com <245545357@qq.com>
2017-07-15 12:02:55 +0800

    Initial Commit

---
+++ ALFrameProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ALFrameProject
+//
+//  Created by 张小明 on 2017/7/15.
+//  Copyright © 2017年 张小明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ALFrameProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ALFrameProject
+//
+//  Created by 张小明 on 2017/7/15.
+//  Copyright © 2017年 张小明. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
